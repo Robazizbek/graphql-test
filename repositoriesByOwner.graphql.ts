f4d4c057afe0852f525916cce1b408fbfc4fae03
Robazizbek <125056869+Robazizbek@users.noreply.github.com>
2025-02-13 05:55:25 -0800

    removed any

---
+++ repositoriesByOwner.graphql.ts
@@ -0,0 +1,146 @@
+import * as Types from '../../../../__generated__/graphql';
+
+import { gql } from '@apollo/client';
+import { RepositoryFragmentDoc } from '../fragments/repository.graphql';
+import * as Apollo from '@apollo/client';
+const defaultOptions = {} as const;
+export type RepositoriesByOwnerQueryVariables = Types.Exact<{
+  login: Types.Scalars['String']['input'];
+  after?: Types.Scalars['String']['input'];
+}>;
+
+export type RepositoriesByOwnerQuery = {
+  __typename?: 'Query';
+  repositoryOwner?:
+    | {
+        __typename?: 'Organization';
+        repositories: {
+          __typename?: 'RepositoryConnection';
+          nodes?: Array<{
+            __typename: 'Repository';
+            id: string;
+            name: string;
+            description?: string | null;
+            url: string;
+            updatedAt: string;
+          } | null> | null;
+          pageInfo: {
+            endCursor: string | null;
+            hasNextPage: boolean;
+          };
+        };
+      }
+    | {
+        __typename?: 'User';
+        repositories: {
+          __typename?: 'RepositoryConnection';
+          nodes?: Array<{
+            __typename: 'Repository';
+            id: string;
+            name: string;
+            description?: string | null;
+            url: string;
+            updatedAt: string;
+          } | null> | null;
+          pageInfo: {
+            endCursor: string | null;
+            hasNextPage: boolean;
+          };
+        };
+      }
+    | null;
+};
+
+export const RepositoriesByOwnerDocument = gql`
+  query repositoriesByOwner($login: String!, $after: String) {
+    repositoryOwner(login: $login) {
+      repositories(first: 10, after: $after) {
+        nodes {
+          ...Repository
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+      }
+    }
+  }
+  ${RepositoryFragmentDoc}
+`;
+
+/**
+ * __useRepositoriesByOwnerQuery__
+ *
+ * To run a query within a React component, call `useRepositoriesByOwnerQuery` and pass it any options that fit your needs.
+ * When your component renders, `useRepositoriesByOwnerQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useRepositoriesByOwnerQuery({
+ *   variables: {
+ *      login: // value for 'login'
+ *   },
+ * });
+ */
+export function useRepositoriesByOwnerQuery(
+  baseOptions: Apollo.QueryHookOptions<
+    RepositoriesByOwnerQuery,
+    RepositoriesByOwnerQueryVariables
+  > &
+    (
+      | { variables: RepositoriesByOwnerQueryVariables; skip?: boolean }
+      | { skip: boolean }
+    )
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+
+  return Apollo.useQuery<
+    RepositoriesByOwnerQuery,
+    RepositoriesByOwnerQueryVariables
+  >(RepositoriesByOwnerDocument, options);
+}
+
+export function useRepositoriesByOwnerLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<
+    RepositoriesByOwnerQuery,
+    RepositoriesByOwnerQueryVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<
+    RepositoriesByOwnerQuery,
+    RepositoriesByOwnerQueryVariables
+  >(RepositoriesByOwnerDocument, options);
+}
+export function useRepositoriesByOwnerSuspenseQuery(
+  baseOptions?:
+    | Apollo.SkipToken
+    | Apollo.SuspenseQueryHookOptions<
+        RepositoriesByOwnerQuery,
+        RepositoriesByOwnerQueryVariables
+      >
+) {
+  const options =
+    baseOptions === Apollo.skipToken
+      ? baseOptions
+      : { ...defaultOptions, ...baseOptions };
+  return Apollo.useSuspenseQuery<
+    RepositoriesByOwnerQuery,
+    RepositoriesByOwnerQueryVariables
+  >(RepositoriesByOwnerDocument, options);
+}
+export type RepositoriesByOwnerQueryHookResult = ReturnType<
+  typeof useRepositoriesByOwnerQuery
+>;
+export type RepositoriesByOwnerLazyQueryHookResult = ReturnType<
+  typeof useRepositoriesByOwnerLazyQuery
+>;
+export type RepositoriesByOwnerSuspenseQueryHookResult = ReturnType<
+  typeof useRepositoriesByOwnerSuspenseQuery
+>;
+export type RepositoriesByOwnerQueryResult = Apollo.QueryResult<
+  RepositoriesByOwnerQuery,
+  RepositoriesByOwnerQueryVariables
+>;
